c077ba774252a033e2708c6495ef937b15359678
WarriorGW <ferguerrero.ordonez@gmail.com>
2023-09-24 22:55:22 -0600

    AddQuestion v1

--- src/pages/AddQuestion.js
+++ src/pages/AddQuestion.js
@@ -1,26 +1,85 @@
 import React from "react";
 import { Formik, Form } from "formik";
 
+import "./css/AddQuestionStyle.css";
+
 function AddQuestion() {
 	return (
-		<div className="container">
+		<div className="Add-container">
 			<Formik>
 				{({ handleChange, handleSubmit, isSubmitting }) => (
 					<Form>
-						<input type="text" placeholder="Pregunta..." />
-						<div>
-							<input type="text" placeholder="Respuesta correcta..." />
-							<input type="text" placeholder="Respuesta incorrecta..." />
-							<input type="text" placeholder="Respuesta incorrecta..." />
-							<input type="text" placeholder="Respuesta incorrecta..." />
+						<div className="card-Add">
+							<div className="input-container">
+								<input
+									className="Add-Text"
+									type="text"
+									placeholder="Ingresar la preguntas deseada "
+								/>
+							</div>
+							<div className="grid-container">
+								<input
+									type="text"
+									className="grid-item"
+									placeholder="Correcta"
+									style={{
+										backgroundImage:
+											'url("https://img.icons8.com/plasticine/100/book.png")', // Ruta de la imagen
+										backgroundSize: "38px 38px",
+										backgroundRepeat: "no-repeat",
+										paddingLeft: "10px",
+										backgroundPosition: "left 10px bottom 10px",
+									}}
+								/>
+								<input
+									type="text"
+									className="grid-item"
+									placeholder="Incorrecta"
+									style={{
+										backgroundImage:
+											'url("https://img.icons8.com/color/48/square-root.png")', // Ruta de la imagen
+										backgroundSize: "38px 38px",
+										backgroundRepeat: "no-repeat",
+										paddingLeft: "10px",
+										backgroundPosition: "left 10px bottom 10px",
+									}}
+								/>
+								<input
+									type="text"
+									className="grid-item"
+									placeholder="Incorrecta"
+									style={{
+										backgroundImage:
+											'url("https://img.icons8.com/external-kiranshastry-solid-kiranshastry/64/7950F2/external-poker-chip-casino-kiranshastry-solid-kiranshastry-5.png")',
+										backgroundSize: "38px 38px",
+										backgroundRepeat: "no-repeat",
+										paddingLeft: "10px",
+										backgroundPosition: "left 10px bottom 10px",
+									}}
+								/>
+								<input
+									type="text"
+									className="grid-item"
+									placeholder="Incorrecta"
+									style={{
+										backgroundImage:
+											'url("https://img.icons8.com/arcade/64/movie-projector.png")',
+										backgroundSize: "38px 38px",
+										backgroundRepeat: "no-repeat",
+										paddingLeft: "10px",
+										backgroundPosition: "left 10px bottom 10px",
+									}}
+								/>
+							</div>
+							<div className="centered-button">
+								<button className="button-Add" type="submit">
+									Guardar pregunta
+								</button>
+							</div>
 						</div>
-						<button type="submit">Guardar pregunta</button>
 					</Form>
 				)}
 			</Formik>
-			<div>
-				<p>Aqui estaran los puntajes uwu</p>
-			</div>
 		</div>
 	);
 }
