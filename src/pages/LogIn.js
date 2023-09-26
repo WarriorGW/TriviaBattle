330ee5b72775741f1411c4b04a0ff7625103f281
WarriorGW <ferguerrero.ordonez@gmail.com>
2023-09-26 07:39:38 -0600

    Estilo LogIn

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,16 +1,29 @@
 import React from "react";
 import { Formik, Form } from "formik";
+import "./css/LogInStyle.css";
 
 function LogIn() {
 	return (
-		<div className="container">
+		<div className="container-login">
 			<Formik>
 				{({ handleChange, handleSubmit, isSubmitting }) => (
 					<Form>
-						<h1>Iniciar Sesion</h1>
-						<input type="text" placeholder="Nombre de usuario..." />
-						<input type="password" placeholder="Contrasena..." />
-						<button type="submit">Iniciar Sesion</button>
+						<div className="card-login">
+							<h1>Iniciar Sesion</h1>
+							<input
+								type="text"
+								className="input-login"
+								placeholder="Ingresar usuario.."
+							/>
+							<input
+								type="password"
+								className="input-login"
+								placeholder="Ingresar Contraseña.."
+							/>
+							<button type="submit" className="button-purple input-login">
+								Iniciar Sesion
+							</button>
+						</div>
 					</Form>
 				)}
 			</Formik>
