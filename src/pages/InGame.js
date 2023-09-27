c03508a0aea76c2246be45543897c299df191717
WarriorGW <ferguerrero.ordonez@gmail.com>
2023-09-27 09:01:24 -0600

    Lesser Error InGame

--- src/pages/InGame.js
+++ src/pages/InGame.js
@@ -15,23 +15,23 @@ function InGame() {
 				<div className="row justify-content-center align-content-center">
 					<div className="box-ingame mt-md-5 col-10 d-flex border-and-shadow px-5 py-3 py-md-5 justify-content-center align-content-center">
 						<div className="w-100">
-							<div className="d-flex text-center">
+							<div className="d-flex justify-content-center text-center">
 								<h2 className="question-title">
 									<>¿</>Cual es la capital de francia<>?</>
 								</h2>
 							</div>
 							<div className="row w-100 mx-auto">
 								<div className="col-md-6 mb-2 mt-1 mb-sm-2 mt-sm-2 mb-md-3 mt-md-4">
-									<Answer text="Respuesta 1" />
+									<Answer text="Bretaña" />
 								</div>
 								<div className="col-md-6 mb-2 mt-1 mb-sm-2 mt-sm-2 mb-md-3 mt-md-4">
-									<Answer text="Respuesta 2" />
+									<Answer text="París" />
 								</div>
 								<div className="col-md-6 mb-2 mt-1 mb-sm-2 mt-sm-2 mt-md-3">
-									<Answer text="Respuesta 3" />
+									<Answer text="Auvernia" />
 								</div>
 								<div className="col-md-6 mb-2 mt-1 mb-sm-2 mt-sm-2 mt-md-3">
-									<Answer text="Respuesta 4" />
+									<Answer text="Lorraine" />
 								</div>
 							</div>
 						</div>
