e596cec26b022f7297d3a1b7e62a2fde6724380d
WarriorGW <ferguerrero.ordonez@gmail.com>
2023-10-02 09:21:46 -0600

    Validacion AddQuestion

--- src/pages/AddQuestion.js
+++ src/pages/AddQuestion.js
@@ -37,6 +37,27 @@ function AddQuestion() {
 							: Swal.fire("Cancelado", "Corrige la pregunta", "error");
 					});
 				}}
+				validate={(values) => {
+					let errors = {};
+					const validateField = (fieldName, value, maxLength) => {
+						if (!value) {
+							errors[fieldName] = "Ingresa una respuesta";
+						} else if (/[^a-zA-Z0-9\sñÑ]/.test(value)) {
+							errors[fieldName] = "No se permiten signos en la respuesta";
+						} else if (value.length > maxLength) {
+							errors[
+								fieldName
+							] = `La respuesta debe tener menos de ${maxLength} caracteres`;
+						}
+					};
+
+					validateField("question", values.question, 500);
+					validateField("correctAnswer", values.correctAnswer, 100);
+					validateField("incorrectAnswer1", values.incorrectAnswer1, 100);
+					validateField("incorrectAnswer2", values.incorrectAnswer2, 100);
+					validateField("incorrectAnswer3", values.incorrectAnswer3, 100);
+					return errors;
+				}}
 			>
 				{({
 					handleChange,
@@ -57,41 +78,75 @@ function AddQuestion() {
 									placeholder="Ingresar la pregunta sin signos de interrogación"
 									value={values.question}
 									onChange={handleChange}
+									onBlur={handleBlur}
 								/>
+								{errors.question && touched.question && (
+									<p className="error-validation">{errors.question}</p>
+								)}
 							</div>
 							<div className="container-grid">
-								<input
-									className="grid-item img-bg-1"
-									type="text"
-									name="correctAnswer"
-									placeholder="Correcta"
-									value={values.correctAnswer}
-									onChange={handleChange}
-								/>
-								<input
-									className="grid-item img-bg-2"
-									type="text"
-									name="incorrectAnswer1"
-									placeholder="Incorrecta"
-									value={values.incorrectAnswer1}
-									onChange={handleChange}
-								/>
-								<input
-									className="grid-item img-bg-3"
-									type="text"
-									name="incorrectAnswer2"
-									placeholder="Incorrecta"
-									value={values.incorrectAnswer2}
-									onChange={handleChange}
-								/>
-								<input
-									className="grid-item img-bg-4"
-									type="text"
-									name="incorrectAnswer3"
-									placeholder="Incorrecta"
-									value={values.incorrectAnswer3}
-									onChange={handleChange}
-								/>
+								<div>
+									<input
+										className="grid-item img-bg-1"
+										type="text"
+										name="correctAnswer"
+										placeholder="Correcta"
+										value={values.correctAnswer}
+										onChange={handleChange}
+										onBlur={handleBlur}
+									/>
+									{errors.correctAnswer && touched.correctAnswer && (
+										<p className="error-validation">{errors.correctAnswer}</p>
+									)}
+								</div>
+								<div>
+									<input
+										className="grid-item img-bg-2"
+										type="text"
+										name="incorrectAnswer1"
+										placeholder="Incorrecta"
+										value={values.incorrectAnswer1}
+										onChange={handleChange}
+										onBlur={handleBlur}
+									/>
+									{errors.incorrectAnswer1 && touched.incorrectAnswer1 && (
+										<p className="error-validation">
+											{errors.incorrectAnswer1}
+										</p>
+									)}
+								</div>
+								<div>
+									<input
+										className="grid-item img-bg-3"
+										type="text"
+										name="incorrectAnswer2"
+										placeholder="Incorrecta"
+										value={values.incorrectAnswer2}
+										onChange={handleChange}
+										onBlur={handleBlur}
+									/>
+									{errors.incorrectAnswer2 && touched.incorrectAnswer2 && (
+										<p className="error-validation">
+											{errors.incorrectAnswer2}
+										</p>
+									)}
+								</div>
+								<div>
+									<input
+										className="grid-item img-bg-4"
+										type="text"
+										name="incorrectAnswer3"
+										placeholder="Incorrecta"
+										value={values.incorrectAnswer3}
+										onChange={handleChange}
+										onBlur={handleBlur}
+									/>
+									{errors.incorrectAnswer3 && touched.incorrectAnswer3 && (
+										<p className="error-validation">
+											{errors.incorrectAnswer3}
+										</p>
+									)}
+								</div>
 							</div>
 							<div className="centered-button">
 								<button
