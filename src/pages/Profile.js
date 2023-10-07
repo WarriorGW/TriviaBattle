732a99e334b2fb8d865d10578b4731d8e3865ad9
WarriorGW <ferguerrero.ordonez@gmail.com>
2023-10-06 22:10:06 -0600

    Mensaje al cerrar sesion

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,26 @@
 import React from "react";
 
 import { Link } from "react-router-dom";
-
+import Swal from "sweetalert2";
 import useAuthStore from "../context/AuthStore";
 
 function Profile() {
 	const authLogOut = useAuthStore((state) => state.authLogout);
 	const authImg = useAuthStore((state) => state.authImg);
+	const handleLogout = () => {
+		Swal.fire({
+			text: "¿Cerrar sesión?",
+			icon: "question",
+			showCancelButton: true,
+			confirmButtonText: "Sí, cerrar sesión",
+			cancelButtonText: "Cancelar",
+		}).then((result) => {
+			if (result.isConfirmed) {
+				// Si el usuario confirma la acción, llama a la función de cierre de sesión
+				authLogOut();
+			}
+		});
+	};
 	return (
 		<div className="card-form-user">
 			{authImg ? (
@@ -22,7 +36,7 @@ function Profile() {
 			<br />
 			<Link to="/ChooseProfileImg">Seleccionar foto de perfil</Link>
 			<br />
-			<button className="button-purple send-button-user" onClick={authLogOut}>
+			<button className="button-purple send-button-user" onClick={handleLogout}>
 				Cerrar sesion
 			</button>
 		</div>
