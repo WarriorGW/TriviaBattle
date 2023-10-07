6561eef4ec680ca7f00b1a8f4cf2fbdfea8c69f3
WarriorGW <ferguerrero.ordonez@gmail.com>
2023-10-07 11:03:25 -0600

    Alerta al cambiar img

--- src/pages/ChooseProfileImg.js
+++ src/pages/ChooseProfileImg.js
@@ -2,8 +2,11 @@ import React from "react";
 import "./css/ChooseProfileImgStyle.css";
 import { useUserStore } from "../context/UserStore";
 import useAuthStore from "../context/AuthStore";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
 
 function ChooseProfileImg() {
+	const navigate = useNavigate();
 	const updateUserImg = useUserStore((state) => state.updateUserImg);
 	const authId = useAuthStore((state) => state.authId);
 	const imagesContext = require.context(
@@ -21,21 +24,36 @@ function ChooseProfileImg() {
 	if (imageNames.includes("user-default")) {
 		imageNames.splice(imageNames.indexOf("user-default"), 1);
 	}
+
+	const handleProfileImg = async (imageName) => {
+		console.log(parseInt(authId, 10), imageName);
+		const response = await updateUserImg(parseInt(authId, 10), imageName);
+		if (response.status === 200) {
+			localStorage.setItem("authImg", imageName);
+		}
+	};
 	return (
 		<div className="card-profiles mt-3 mb-5 mt-lg-5">
 			{imageNames.map((imageName, index) => (
 				<button
 					key={index}
 					className="button-profiles-selector m-2"
-					onClick={async () => {
-						console.log(parseInt(authId, 10), imageName);
-						const response = await updateUserImg(
-							parseInt(authId, 10),
-							imageName
-						);
-						if (response.status === 200) {
-							localStorage.setItem("authImg", imageName);
-						}
+					onClick={() => {
+						// Mostrar la imagen seleccionada en la alerta
+						Swal.fire({
+							title: "¿Seguro que quieres esta imagen?",
+							imageUrl: require(`../assets/profiles/${imageName}.png`),
+							imageAlt: `Profile: ${imageName}`,
+							showCancelButton: true,
+							confirmButtonText: "Sí",
+							cancelButtonText: "Cancelar",
+						}).then((result) => {
+							if (result.isConfirmed) {
+								// Si el usuario confirma la acción, llama a la función de cierre de sesión
+								handleProfileImg(imageName);
+								navigate("/");
+							}
+						});
 					}}
 				>
 					<img
