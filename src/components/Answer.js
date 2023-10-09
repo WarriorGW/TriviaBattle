6a79e5debacb7d029316b27c91ba8fa9719c96c7
WarriorGW <ferguerrero.ordonez@gmail.com>
2023-10-09 09:33:52 -0600

    Answer abarca mas

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -4,9 +4,11 @@ import "./css/AnswerStyle.css";
 
 function Answer(props) {
 	return (
-		<button className="answer-container border-and-shadow col-6">
-			{props.text}
-		</button>
+		<div className="col-md-6 mb-2 mt-1 mb-sm-3 mt-sm-2 mt-md-3">
+			<button className="answer-container border-and-shadow col-6">
+				{props.text}
+			</button>{" "}
+		</div>
 	);
 }
 
