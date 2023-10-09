4688445cb22095d0aee5f1969a5e0787377285ff
WarriorGW <ferguerrero.ordonez@gmail.com>
2023-10-09 09:32:43 -0600

    UserScore mejorado

--- src/components/UsersScore.js
+++ src/components/UsersScore.js
@@ -3,15 +3,17 @@ import "./css/UsersScoreStyle.css";
 
 function UsersScore(props) {
 	return (
-		<div className="user-score-container d-flex border-and-shadow mb-3 mb-md-4 mx-1">
-			<img
-				className="img-users"
-				alt="profile"
-				src={require(`../assets/profiles/${props.image}.png`)}
-			/>
-			<p className="m-0">
-				{props.name} ● {props.score}
-			</p>
+		<div className="col-4 m-0 p-0">
+			<div className="user-score-container d-flex border-and-shadow mb-3 mb-md-4 mx-1">
+				<img
+					className="img-users"
+					alt="profile"
+					src={require(`../assets/profiles/${props.image}.png`)}
+				/>
+				<p className="m-0">
+					{props.name} ● {props.score}
+				</p>
+			</div>
 		</div>
 	);
 }
