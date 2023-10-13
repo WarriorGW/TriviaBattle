8ab2e8e892d4e7abd9c9ae7ed3e8696de79f8d5b
WarriorGW <ferguerrero.ordonez@gmail.com>
2023-10-13 12:33:06 -0600

    Tablas usuario y preguntas

--- src/tables/QuestionTable.js
+++ src/tables/QuestionTable.js
@@ -7,30 +7,38 @@ function QuestionTable() {
 		getAllQuestions();
 	}, [getAllQuestions]);
 	return (
-		<table className="table-dark">
-			<thead>
-				<tr>
-					<th scope="col">#</th>
-					<th scope="col">Pregunta</th>
-					<th scope="col">Correcta</th>
-					<th scope="col">Incorrecta1</th>
-					<th scope="col">Incorrecta2</th>
-					<th scope="col">Incorrecta3</th>
-				</tr>
-			</thead>
-			<tbody>
-				{questions.map((question, index) => (
-					<tr key={index}>
-						<th scope="row">{question.id_question}</th>
-						<td>{question.question}</td>
-						<td>{question.right_answer}</td>
-						<td>{question.wrong_answer_1}</td>
-						<td>{question.wrong_answer_2}</td>
-						<td>{question.wrong_answer_3}</td>
-					</tr>
-				))}
-			</tbody>
-		</table>
+		<div className="table-admin-1">
+			<div className="table-admin-2">
+				<div className="table-admin-3">
+					<table className="table table-hover table-striped m-0">
+						<thead>
+							<tr>
+								<th scope="col">#</th>
+								<th scope="col" className="w-100">
+									Pregunta
+								</th>
+								<th scope="col">Correcta</th>
+								<th scope="col">Incorrecta</th>
+								<th scope="col">Incorrecta</th>
+								<th scope="col">Incorrecta</th>
+							</tr>
+						</thead>
+						<tbody>
+							{questions.map((question, index) => (
+								<tr key={index}>
+									<th scope="row">{question.id_question}</th>
+									<td>{question.question}</td>
+									<td>{question.right_answer}</td>
+									<td>{question.wrong_answer_1}</td>
+									<td>{question.wrong_answer_2}</td>
+									<td>{question.wrong_answer_3}</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
 	);
 }
 
--- src/tables/UserTable.js
+++ src/tables/UserTable.js
@@ -2,32 +2,45 @@ import React, { useEffect } from "react";
 
 import { useUserStore } from "../context/UserStore";
 
+import "./TablesStyles.css";
+
 function UserTable() {
 	const { users, getAllUsers } = useUserStore();
 
 	useEffect(() => {
-		getAllUsers();
+		const fetch = async () => {
+			await getAllUsers();
+		};
+		fetch();
 	}, [getAllUsers]);
 
 	return (
-		<table className="table-dark">
-			<thead>
-				<tr>
-					<th scope="col">#</th>
-					<th scope="col">Username</th>
-					<th scope="col">Password</th>
-				</tr>
-			</thead>
-			<tbody>
-				{users.map((user, index) => (
-					<tr key={index}>
-						<th scope="row">{user.id_user}</th>
-						<td>{user.username}</td>
-						<td>{user.password}</td>
-					</tr>
-				))}
-			</tbody>
-		</table>
+		<div className="table-admin-1">
+			<div className="table-admin-2">
+				<div className="table-admin-3">
+					<table className="table table-hover table-striped m-0">
+						<thead>
+							<tr>
+								<th scope="col">#</th>
+								<th scope="col">Nombre</th>
+								<th scope="col">Puntos</th>
+								<th scope="col">Rol</th>
+							</tr>
+						</thead>
+						<tbody>
+							{users.map((user, index) => (
+								<tr key={index}>
+									<th scope="row">{user.id_user}</th>
+									<td>{user.username}</td>
+									<td>{user.password}</td>
+									<td>{user.role === 2 ? "admin" : "usuario"}</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
 	);
 }
 
