e305c0e06d70a2dbdba7e7a412dbe4f9e4dccaca
WarriorGW <ferguerrero.ordonez@gmail.com>
2023-10-16 20:04:41 -0600

    Store de todas las apis

--- src/context/ScoresStore.js
+++ src/context/ScoresStore.js
@@ -1,6 +1,11 @@
 import { create } from "zustand";
 
-import { getAllScoresReq } from "../api/scores.api";
+import {
+	getAllScoresReq,
+	getOneScoreReq,
+	updateScoreReq,
+	createScoreReq,
+} from "../api/scores.api";
 
 export const useScoreStore = create((set) => ({
 	scores: [],
@@ -9,6 +14,35 @@ export const useScoreStore = create((set) => ({
 			const response = await getAllScoresReq();
 			set({ scores: response.data });
 			return response;
-		} catch (error) {}
+		} catch (error) {
+			console.error("Error al obtener puntajes:", error);
+		}
+	},
+	getOneScore: async (id_user) => {
+		try {
+			const response = await getOneScoreReq(id_user);
+			set({ score: response.data });
+			return response;
+		} catch (error) {
+			console.error("Error al obtener puntaje:", error);
+		}
+	},
+	createScore: async (id_user, score) => {
+		try {
+			const response = await createScoreReq(id_user, score);
+			set({ score: response });
+			return response;
+		} catch (error) {
+			console.error("Error al crear puntaje:", error);
+		}
+	},
+	updateScore: async (id_user, newScore) => {
+		try {
+			const response = await updateScoreReq(id_user, newScore);
+			set({ score: response });
+			return response;
+		} catch (error) {
+			console.error("Error al actualizar puntaje:", error);
+		}
 	},
 }));
