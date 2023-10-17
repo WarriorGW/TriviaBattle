ef235738681bf8faa00b9faec8b4644d6ce81787
WarriorGW <ferguerrero.ordonez@gmail.com>
2023-10-16 20:04:19 -0600

    Agregadas las apis

--- src/api/scores.api.js
+++ src/api/scores.api.js
@@ -4,3 +4,15 @@ import BASE_URL from "./base_url";
 export const getAllScoresReq = async () => {
 	return await axios.get(`${BASE_URL}/scores`);
 };
+
+export const getOneScoreReq = async (id_user) => {
+	return await axios.post(`${BASE_URL}/score`, { id_user });
+};
+
+export const createScoreReq = async (id_user, score) => {
+	return await axios.post(`${BASE_URL}/scores`, { id_user, score });
+};
+
+export const updateScoreReq = async (id_user, score) => {
+	return await axios.put(`${BASE_URL}/scores`, { score, id_user });
+};
