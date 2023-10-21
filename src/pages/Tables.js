dc93ac900981ba7558005b2b99e9f3cbd93eb63e
WarriorGW <ferguerrero.ordonez@gmail.com>
2023-10-21 14:25:07 -0600

    Tables bonito

--- src/pages/Tables.js
+++ src/pages/Tables.js
@@ -1,18 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import users from "../assets/tables/users.svg";
+import questions from "../assets/tables/questions.svg";
+
 function Tables() {
 	return (
 		<>
-			<div className="container">
-				<h1>Tablas</h1>
-				<Link to="/Tables/users">AllUsers</Link>
-				<br />
-				<Link to="/Tables/users/:id">OneUser</Link>
-				<br />
-				<Link to="/Tables/questions">AllQuestions</Link>
-				<br />
-				<Link to="/Tables/questions/:id">OneQuestion</Link>
+			<div className="container d-flex flex-wrap text-center justify-content-center">
+				<div className="w-100">
+					<h1>Tablas</h1>
+				</div>
+				<Link to="/Tables/users" className="d-flex flex-column m-4">
+					<img width="128" height="128" src={users} alt="users table" />
+					AllUsers
+				</Link>
+				<Link to="/Tables/questions" className="d-flex flex-column m-4">
+					<img width="128" height="128" src={questions} alt="questions table" />
+					AllQuestions
+				</Link>
 			</div>
 		</>
 	);
