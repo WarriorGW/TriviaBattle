09ab19a222ad8df1c5986c5ac0aff4c021a20126
WarriorGW <ferguerrero.ordonez@gmail.com>
2023-10-21 14:55:31 -0600

    Sad-Dizzy svg

--- src/pages/UnAuthorized.js
+++ src/pages/UnAuthorized.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./css/Error404Style.css";
 
+import sadFace from "../assets/sad-dizzy.svg";
+
 function UnAuthorized() {
 	return (
 		<div className="container-error">
-			<i className="uil uil-sad-dizzy" style={{ fontSize: "200px" }} />
+			{/* <i className="uil uil-sad-dizzy" style={{ fontSize: "200px" }} /> */}
+			<img width={200} height={200} src={sadFace} alt="Acceso no autorizado" />
 			<h1 className="title-error">401</h1>
 			<hr />
 			<h2 className="subtitle-error">Usuario sin autorizacion</h2>
