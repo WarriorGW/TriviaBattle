85b16582425d5e52fd9c2a8d5c32e42f9b23db17
WarriorGW <ferguerrero.ordonez@gmail.com>
2023-10-21 16:27:23 -0600

    Fix hitbox LinkCards

--- src/components/LinkCard.js
+++ src/components/LinkCard.js
@@ -5,8 +5,8 @@ import "./css/LinkCardStyle.css";
 
 function LinkCard(props) {
 	return (
-		<Link to={props.to}>
-			<div className="link-card d-flex flex-column m-4">
+		<Link to={props.to} className="m-4">
+			<div className="link-card d-flex flex-column">
 				<img width="128" height="128" src={props.image} alt={props.text} />
 				<h2>{props.text}</h2>
 			</div>
